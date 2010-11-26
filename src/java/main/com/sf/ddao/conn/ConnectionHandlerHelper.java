c715e01f86eb5c81c06bbcedf5d8d7f0b019939e
psyrtsov <psyrtsov@gmail.com>
2010-11-26 16:55:29 +0100

    updated copywrite

--- src/java/main/com/sf/ddao/conn/ConnectionHandlerHelper.java
+++ src/java/main/com/sf/ddao/conn/ConnectionHandlerHelper.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright 2008 Pavel Syrtsov
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -7,11 +7,11 @@
  *
  *    http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and limitations
- * under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and limitations
+ *  under the License.
  */
 package com.sf.ddao.conn;
 
@@ -26,6 +26,9 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 /**
+ * ConnectionHandlerHelper helps to deal with connection allocation and transactions handling
+ * it keeps connection in context for autocommit mode and in thread local variable when using transactions.
+ * <p/>
  * Created by: pavel
  * Date: Jun 11, 2008
  * Time: 10:50:45 PM
--- test/com/sf/ddao/alinker/CtxBuilderTest.java
+++ test/com/sf/ddao/alinker/CtxBuilderTest.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright 2008 Pavel Syrtsov
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -7,11 +7,11 @@
  *
  *    http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and limitations
- * under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and limitations
+ *  under the License.
  */
 
 package com.sf.ddao.alinker;
@@ -58,7 +58,7 @@ public class CtxBuilderTest extends TestCase {
     private static class MyInjectorTest {
         String injectedValue = null;
 
-        public void setInjectedValue(@InjectValue(INJECTED_VALUE)String injectedValue) {
+        public void setInjectedValue(@InjectValue(INJECTED_VALUE) String injectedValue) {
             this.injectedValue = injectedValue;
         }
     }
